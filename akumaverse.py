d79aa21f97e91d9286a1cbe7dd21f0e3f39375d4
Sunny <groebnermelissa@gmail.com>
2025-08-04 19:17:16 +0200

    First WIP

---
+++ akumaverse.py
@@ -0,0 +1,34 @@
+akumaverse = [
+    {"horny":"Mi'Libarian"},
+    {"horny":"Mi'Boss"},
+    {"wholesome":"Mi'Cowboy"}
+    ]
+
+def kiss():
+    print('You gave him a kiss on the cheek.')
+
+def blowie():
+    print('He came in your mouth.')
+
+
+# if akumaverse.keys() == "horny":
+for asmr in akumaverse.values():
+    print(akumaverse.values())
+    if akumaverse.values() == "Mi'Libarian":
+        print("You provoced the handsome librarian.")
+        print('Puppy lover <3')
+        kiss
+    elif akumaverse.values == "Mi'Boss":
+        print("You got locked into a closet with your boss at work.")
+        print('Gawk Gawk Incident')
+        blowie
+    else:
+        print("You probably died.")
+
+
+
+
+
+    
+
+
